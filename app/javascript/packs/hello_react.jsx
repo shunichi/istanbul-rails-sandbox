26287700bb90e1734e18c79c20b65a5571cfdc1c
Shunichi Ikegami <sike.tm@gmail.com>
2019-08-14 17:38:43 +0900

    Modify hello_react.jsx

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,21 +6,29 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const Hello = ({name, bye}) => {
+  if (bye) {
+    return <div>Bye {name}!</div>;
+  } else {
+    return <div>Hello {name}!</div>;
+  }
+};
 
 Hello.defaultProps = {
-  name: 'David'
-}
+  name: 'David',
+  bye: false,
+};
 
 Hello.propTypes = {
-  name: PropTypes.string
-}
+  name: PropTypes.string,
+  bye: PropTypes.bool,
+};
 
 document.addEventListener('DOMContentLoaded', () => {
+  const elem = document.getElementById('hello-data')
+  const bye = elem && elem.dataset.greeting == 'bye'
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello name="React" bye={bye} />,
     document.body.appendChild(document.createElement('div')),
   )
-})
+});
--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Home', type: :system do
-  it 'visit root' do
+  it 'visit root', js: true do
     visit root_path
     expect(page).to have_content 'Hello, MyApp'
     expect(page).to have_content 'Hello React!'
+  end
+
+  it 'visit bye root', js: true do
+    visit root_path(greeting: 'bye')
+    expect(page).to have_content 'Hello, MyApp'
+    expect(page).to have_content 'Bye React!'
   end
 end
